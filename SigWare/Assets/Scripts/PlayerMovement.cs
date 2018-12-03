eae412fad7a9b3aea3af4ae1726a627e3f798f66
Geoffrey <g.pineau@rubika-edu.com>
2018-12-03 19:49:23 +0100

    Avancées SigWare

---
+++ SigWare/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+    public Camera cam;
+
+    public NavMeshAgent agent;
+
+    public Slider batterySlider;
+
+    public float speed;
+
+    public bool canMove;
+
+    private int plug = 0 ;
+
+    public float batteryCharge = 0.7f;
+
+    public float batteryValue = 0.7f;
+
+
+    public void Start()
+    {
+        //batterySlider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+        batterySlider.value = batteryCharge;
+    }
+
+    void Update()
+    {
+        PlugUnplug();       //Fonction stop/démarrer
+        Charging();         //Fonction rechargement
+
+        batterySlider.value = batterySlider.value - 0.001f;     //Diminution du slider
+
+        if (canMove == true && plug == 0)
+        {
+            PlayerMovementNav();
+            Debug.Log("is moving");
+        }
+    }
+
+    private void PlugUnplug()
+    {
+        if (Input.GetMouseButtonDown(0) && plug == 0)       //Clic gauche pour Stop
+        {
+            canMove = false;
+            Debug.Log("plugged");
+            gameObject.GetComponent<NavMeshAgent>().isStopped = true;
+            plug = 1;
+            
+        }
+
+        if (Input.GetMouseButtonDown(1) && plug == 1)       //Clic droit pour Démarrer
+        {
+            canMove = true;
+            plug = 0;
+            Debug.Log("unplugged");
+            gameObject.GetComponent<NavMeshAgent>().isStopped = false;
+        }
+
+    }
+
+    private void PlayerMovementNav()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            agent.SetDestination(hit.point);
+        }
+    }
+
+    private void Charging()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        if (canMove == false)
+        {
+            batteryValue = Mathf.Abs(mouseX + mouseY);      //Retourne la valeur positive du calcul
+            Debug.Log(batteryValue);
+            //ValueChangeCheck();
+        }
+    }
+
+    /*public void ValueChangeCheck()      //MAJ du slider
+    {
+        Debug.Log(batterySlider.value);
+        batterySlider.value = batteryValue;
+    }*/
+}
