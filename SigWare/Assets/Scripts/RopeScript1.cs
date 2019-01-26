43a9d664f0305ee6d8cd771a8c31e5d0315aefe6
Unknown <g.pineau@rubika-edu.com>
2019-01-26 15:29:08 +0100

    CableSystemDone

--- SigWare/Assets/Scripts/RopeScript1.cs
+++ SigWare/Assets/Scripts/RopeScript1.cs
@@ -1,203 +1,193 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// Require a Rigidbody and LineRenderer object for easier assembly
-[RequireComponent(typeof(Rigidbody))]
-[RequireComponent(typeof(LineRenderer))]
+//Simulate a rope with verlet integration and no springs
 
-public class RopeScript : MonoBehaviour
+public class RopeScript1 : MonoBehaviour
 {
-    /*========================================
-	==  Physics Based Rope				==
-	==  File: Rope.js					  ==
-	==  Original by: Jacob Fletcher		==
-	==  Use and alter Freely			 ==
-	==  CSharp Conversion by: Chelsea Hash  ==
-	==========================================
-	How To Use:
-	 ( BASIC )
-	 1. Simply add this script to the object you want a rope teathered to
-	 2. In the "LineRenderer" that is added, assign a material and adjust the width settings to your likeing
-	 3. Assign the other end of the rope as the "Target" object in this script
-	 4. Play and enjoy!
- 
-	 ( Advanced )
-	 1. Do as instructed above
-	 2. If you want more control over the rigidbody on the ropes end go ahead and manually
-		 add the rigidbody component to the target end of the rope and adjust acordingly.
-	 3. adjust settings as necessary in both the rigidbody and rope script
- 
-	 (About Character Joints)
-	 Sometimes your rope needs to be very limp and by that I mean NO SPRINGY EFFECT.
-	 In order to do this, you must loosen it up using the swingAxis and twist limits.
-	 For example, On my joints in my drawing app, I set the swingAxis to (0,0,1) sense
-	 the only axis I want to swing is the Z axis (facing the camera) and the other settings to around -100 or 100.
- 
- 
-	*/
+    //Objects that will interact with the rope
+    public Transform whatTheRopeIsConnectedTo;
+    public Transform whatIsHangingFromTheRope;
 
-    public Transform target;
-    public float resolution = 0.5F;                           //  Sets the amount of joints there are in the rope (1 = 1 joint for every 1 unit)
-    public float ropeDrag = 0.1F;                                //  Sets each joints Drag
-    public float ropeMass = 0.1F;                           //  Sets each joints Mass
-    public float ropeColRadius = 0.5F;                  //  Sets the radius of the collider in the SphereCollider component
-                                                        //public float ropeBreakForce = 25.0F;					 //-------------- TODO (Hopefully will break the rope in half...
-    private Vector3[] segmentPos;           //  DONT MESS!	This is for the Line Renderer's Reference and to set up the positions of the gameObjects
-    private GameObject[] joints;            //  DONT MESS!	This is the actual joint objects that will be automatically created
-    private LineRenderer line;                          //  DONT MESS!	 The line renderer variable is set up when its assigned as a new component
-    private int segments = 0;                   //  DONT MESS!	The number of segments is calculated based off of your distance * resolution
-    private bool rope = false;                       //  DONT MESS!	This is to keep errors out of your debug window! Keeps the rope from rendering when it doesnt exist...
+    //Line renderer used to display the rope
+    private LineRenderer lineRenderer;
 
-    //Joint Settings
-    public Vector3 swingAxis = new Vector3(1, 1, 1);                 //  Sets which axis the character joint will swing on (1 axis is best for 2D, 2-3 axis is best for 3D (Default= 3 axis))
-    public float lowTwistLimit = -100.0F;                   //  The lower limit around the primary axis of the character joint. 
-    public float highTwistLimit = 100.0F;                   //  The upper limit around the primary axis of the character joint.
-    public float swing1Limit = 20.0F;                   //	The limit around the primary axis of the character joint starting at the initialization point.
+    //A list with all rope section
+    private List<RopeSection> allRopeSections = new List<RopeSection>();
 
-    void Awake()
+    //Rope data
+    private float ropeSectionLength = 0.5f;
+
+    private void Start()
     {
-        BuildRope();
-    }
-
-    void Update()
-    {
-        // Put rope control here!
+        //Init the line renderer we use to display the rope
+        lineRenderer = GetComponent<LineRenderer>();
 
 
-        //Destroy Rope Test	(Example of how you can use the rope dynamically)
-        if (rope && Input.GetKeyDown("d"))
+        //Create the rope
+        Vector3 ropeSectionPos = whatTheRopeIsConnectedTo.position;
+
+        for (int i = 0; i < 15; i++)
         {
-            DestroyRope();
-        }
-        if (!rope && Input.GetKeyDown("r"))
-        {
-            BuildRope();
+            allRopeSections.Add(new RopeSection(ropeSectionPos));
+
+            ropeSectionPos.y -= ropeSectionLength;
         }
     }
-    void LateUpdate()
+
+    private void Update()
     {
-        // Does rope exist? If so, update its position
-        if (rope)
+        //Display the rope with the line renderer
+        DisplayRope();
+
+        //Move what is hanging from the rope to the end of the rope
+        //whatIsHangingFromTheRope.position = allRopeSections[allRopeSections.Count - 1].pos;
+
+        //Make what's hanging from the rope look at the next to last rope position to make it rotate with the rope
+        //whatIsHangingFromTheRope.LookAt(allRopeSections[allRopeSections.Count - 2].pos);
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateRopeSimulation();
+    }
+
+    private void UpdateRopeSimulation()
+    {
+        Vector3 gravityVec = new Vector3(0f, -9.81f, 0f);
+
+        float t = Time.fixedDeltaTime;
+
+
+        //Move the first section to what the rope is hanging from
+        RopeSection firstRopeSection = allRopeSections[0];
+
+        firstRopeSection.pos = whatTheRopeIsConnectedTo.position;
+
+        allRopeSections[0] = firstRopeSection;
+
+
+        //Move the other rope sections with Verlet integration
+        for (int i = 1; i < allRopeSections.Count; i++)
         {
-            for (int i = 0; i < segments; i++)
+            RopeSection currentRopeSection = allRopeSections[i];
+
+            //Calculate velocity this update
+            Vector3 vel = currentRopeSection.pos - currentRopeSection.oldPos;
+
+            //Update the old position with the current position
+            currentRopeSection.oldPos = currentRopeSection.pos;
+
+            //Find the new position
+            currentRopeSection.pos += vel;
+
+            //Add gravity
+            currentRopeSection.pos += gravityVec * t;
+
+            //Add it back to the array
+            allRopeSections[i] = currentRopeSection;
+        }
+
+
+        //Make sure the rope sections have the correct lengths
+        for (int i = 0; i < 20; i++)
+        {
+            ImplementMaximumStretch();
+        }
+    }
+
+    //Make sure the rope sections have the correct lengths
+    private void ImplementMaximumStretch()
+    {
+        for (int i = 0; i < allRopeSections.Count - 1; i++)
+        {
+            RopeSection topSection = allRopeSections[i];
+
+            RopeSection bottomSection = allRopeSections[i + 1];
+
+            //The distance between the sections
+            float dist = (topSection.pos - bottomSection.pos).magnitude;
+
+            //What's the stretch/compression
+            float distError = Mathf.Abs(dist - ropeSectionLength);
+
+            Vector3 changeDir = Vector3.zero;
+
+            //Compress this sections
+            if (dist > ropeSectionLength)
             {
-                if (i == 0)
-                {
-                    line.SetPosition(i, transform.position);
-                }
-                else
-                if (i == segments - 1)
-                {
-                    line.SetPosition(i, target.transform.position);
-                }
-                else
-                {
-                    line.SetPosition(i, joints[i].transform.position);
-                }
+                changeDir = (topSection.pos - bottomSection.pos).normalized;
             }
-            line.enabled = true;
-        }
-        else
-        {
-            line.enabled = false;
+            //Extend this section
+            else if (dist < ropeSectionLength)
+            {
+                changeDir = (bottomSection.pos - topSection.pos).normalized;
+            }
+            //Do nothing
+            else
+            {
+                continue;
+            }
+
+
+            Vector3 change = changeDir * distError;
+
+            if (i != 0)
+            {
+                bottomSection.pos += change * 0.5f;
+
+                allRopeSections[i + 1] = bottomSection;
+
+                topSection.pos -= change * 0.5f;
+
+                allRopeSections[i] = topSection;
+            }
+            //Because the rope is connected to something
+            else
+            {
+                bottomSection.pos += change;
+
+                allRopeSections[i + 1] = bottomSection;
+            }
         }
     }
 
-
-
-    void BuildRope()
+    //Display the rope with a line renderer
+    private void DisplayRope()
     {
-        line = gameObject.GetComponent<LineRenderer>();
+        float ropeWidth = 0.2f;
 
-        // Find the amount of segments based on the distance and resolution
-        // Example: [resolution of 1.0 = 1 joint per unit of distance]
-        segments = (int)(Vector3.Distance(transform.position, target.position) * resolution);
-        line.SetVertexCount(segments);
-        segmentPos = new Vector3[segments];
-        joints = new GameObject[segments];
-        segmentPos[0] = transform.position;
-        segmentPos[segments - 1] = target.position;
+        lineRenderer.startWidth = ropeWidth;
+        lineRenderer.endWidth = ropeWidth;
 
-        // Find the distance between each segment
-        var segs = segments - 1;
-        var seperation = ((target.position - transform.position) / segs);
+        //An array with all rope section positions
+        Vector3[] positions = new Vector3[allRopeSections.Count];
 
-        for (int s = 1; s < segments; s++)
+        for (int i = 0; i < allRopeSections.Count; i++)
         {
-            // Find the each segments position using the slope from above
-            Vector3 vector = (seperation * s) + transform.position;
-            segmentPos[s] = vector;
-
-            //Add Physics to the segments
-            AddJointPhysics(s);
+            positions[i] = allRopeSections[i].pos;
         }
 
-        // Attach the joints to the target object and parent it to this object	
-        CharacterJoint end = target.gameObject.AddComponent<CharacterJoint>();
-        end.connectedBody = joints[joints.Length - 1].transform.rigidbody;
-        end.swingAxis = swingAxis;
-        SoftJointLimit limit_setter = end.lowTwistLimit;
-        limit_setter.limit = lowTwistLimit;
-        end.lowTwistLimit = limit_setter;
-        limit_setter = end.highTwistLimit;
-        limit_setter.limit = highTwistLimit;
-        end.highTwistLimit = limit_setter;
-        limit_setter = end.swing1Limit;
-        limit_setter.limit = swing1Limit;
-        end.swing1Limit = limit_setter;
-        target.parent = transform;
+        lineRenderer.numPositions = positions.Length;
 
-        // Rope = true, The rope now exists in the scene!
-        rope = true;
+        lineRenderer.SetPositions(positions);
     }
 
-    void AddJointPhysics(int n)
+    //A struct that will hold information about each rope section
+    public struct RopeSection
     {
-        joints[n] = new GameObject("Joint_" + n);
-        joints[n].transform.parent = transform;
-        Rigidbody rigid = joints[n].AddComponent<Rigidbody>();
-        SphereCollider col = joints[n].AddComponent<SphereCollider>();
-        CharacterJoint ph = joints[n].AddComponent<CharacterJoint>();
-        ph.swingAxis = swingAxis;
-        SoftJointLimit limit_setter = ph.lowTwistLimit;
-        limit_setter.limit = lowTwistLimit;
-        ph.lowTwistLimit = limit_setter;
-        limit_setter = ph.highTwistLimit;
-        limit_setter.limit = highTwistLimit;
-        ph.highTwistLimit = limit_setter;
-        limit_setter = ph.swing1Limit;
-        limit_setter.limit = swing1Limit;
-        ph.swing1Limit = limit_setter;
-        //ph.breakForce = ropeBreakForce; <--------------- TODO
+        public Vector3 pos;
+        public Vector3 oldPos;
 
-        joints[n].transform.position = segmentPos[n];
+        //To write RopeSection.zero
+        public static readonly RopeSection zero = new RopeSection(Vector3.zero);
 
-        rigid.drag = ropeDrag;
-        rigid.mass = ropeMass;
-        col.radius = ropeColRadius;
-
-        if (n == 1)
+        public RopeSection(Vector3 pos)
         {
-            ph.connectedBody = transform.rigidbody;
-        }
-        else
-        {
-            ph.connectedBody = joints[n - 1].rigidbody;
-        }
+            this.pos = pos;
 
-    }
-
-    void DestroyRope()
-    {
-        // Stop Rendering Rope then Destroy all of its components
-        rope = false;
-        for (int dj = 0; dj < joints.Length - 1; dj++)
-        {
-            Destroy(joints[dj]);
+            this.oldPos = pos;
         }
-
-        segmentPos = new Vector3[0];
-        joints = new GameObject[0];
-        segments = 0;
     }
 }
