b1f8651fd095e5ecbeef8a41d97dda8b68ad56ba
Geoffrey <g.pineau@rubika-edu.com>
2019-01-29 19:08:03 +0100

    Début cinématique

--- SigWare/Assets/Scripts/PriseScript.cs
+++ SigWare/Assets/Scripts/PriseScript.cs
@@ -2,23 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PriseScript : MonoBehaviour {
 
-    public GameObject cable;
-
-    private void OnTriggerEnter(Collider other)
+namespace GR19
+{
+    public class PriseScript : MonoBehaviour
     {
-        if (other.name == "Player")
+
+        public GameObject cable;
+
+        private void OnTriggerEnter(Collider other)
         {
-            cable.SetActive(true);
+            if (other.name == "Player")
+            {
+                cable.SetActive(true);
+            }
         }
-    }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.name == "Player")
+        private void OnTriggerExit(Collider other)
         {
-            cable.SetActive(false);
+            if (other.name == "Player")
+            {
+                cable.SetActive(false);
+            }
         }
     }
 }
