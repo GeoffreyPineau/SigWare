e96dd58c72ef30473a0797e20113072279d34b9d
Unknown <g.pineau@rubika-edu.com>
2019-02-10 18:03:51 +0100

    PushAvecNewEmplacementFichier a ne pas changer

---
+++ SigWare/Assets/GRP19/Scripts/PriseScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GR19
+{
+    public class PriseScript : MonoBehaviour
+    {
+
+        public GameObject cable;
+        public Animator pluganim;
+        public Animator plugUIAnim;
+
+
+        public ParticleSystem particle;
+        public int numberParticle;
+        public GameObject clicSound;
+        public AudioSource fireSound;
+        bool soundPlayed = false;
+        bool particleEmit = false;
+        public float fireVolume = 0.4f;
+        public float fireVolumeStart = 0.1f;
+
+
+        public Animator sickLitAnim;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.name == "Player")
+            {
+                cable.SetActive(true);
+                pluganim.SetInteger("State", 1);
+                plugUIAnim.SetBool("isCharging", true);
+                sickLitAnim.SetBool("girlIsHere", true);
+                if(particleEmit == false)
+                {
+                    particle.Emit(numberParticle);
+                    particleEmit = true;
+                    Debug.Log("EmitGhost");
+                    if(soundPlayed == false)
+                    {
+                        clicSound.SetActive(true);
+                        soundPlayed = true;
+                        fireSound.volume = fireVolume;
+                    }
+                }
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.name == "Player")
+            {
+                cable.SetActive(false);
+                pluganim.SetInteger("State", 2);
+                plugUIAnim.SetBool("isCharging", false);
+                sickLitAnim.SetBool("girlIsHere", false);
+                particleEmit = false;
+                clicSound.SetActive(false);
+                soundPlayed = false;
+                fireSound.volume = fireVolumeStart;
+            }
+        }
+    }
+}
---
+++ SigWare/Assets/GRP19/Scripts/RandomChildMaterial.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GR19
+{
+    public class RandomChildMaterial : MonoBehaviour
+    {
+
+        public Material[] randomMaterials;
+        public GameObject sickKid;
+        public AudioSource ouchCartoon;
+        public AudioSource rollingChair;
+
+        public void ColorKid()
+        {
+
+                sickKid.GetComponent<Renderer>().material = randomMaterials[Random.Range(0, randomMaterials.Length)];
+            /*foreach (GameObject kid in sickKid)
+            { 
+
+            }*/
+        }
+
+        public void PlaySound()
+        {
+            ouchCartoon.Play();
+        }
+
+        public void PlaySoundChair()
+        {
+            rollingChair.Play();
+        }
+    }
+}
+
